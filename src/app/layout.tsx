6908f3adb636ccc6c133c40e50d3b5c5d6f14702
actonaa <actonaputra@gmail.com>
2024-12-06 20:31:09 +0700

    menghapus loading ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth focus:scroll-smooth">
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${playfairDisplay.variable} antialiased`}
       >
