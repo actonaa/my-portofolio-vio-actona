6259c7e792e04be25dc2cc58d2e5a14ac6e14416
actonaa <actonaputra@gmail.com>
2024-12-07 09:16:41 +0700

    Feat: Resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,7 +1,20 @@
 export default function ResumePage() {
   return (
-    <div>
-      <h1>Resume</h1>
+    <div className="bg-darkgreen pt-36 pb-32">
+      <div className="max-w-5xl flex flex-col items-center justify-center mx-auto komputer:max-w-6xl text-white">
+        <a
+          href="/CV-Vio-Actona-Putra.pdf"
+          download="CV-Vio-Actona-Putra.pdf"
+          className="flex w-1/4 justify-center items-center text-lg mb-2 py-2 px-6 bg-orange rounded-full hover:opacity-80"
+        >
+          Download My Resume
+        </a>
+        <iframe
+          src="/CV-Vio-Actona-Putra.pdf"
+          className="w-full h-[500px] border-0 mt-12"
+          title="Resume"
+        ></iframe>
+      </div>
     </div>
   );
 }
